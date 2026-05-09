ccdf8de19c957f517725484ef0f7c141ebbd29cb
TenMountains <2151483295@qq.com>
2026-05-09 17:08:36 +0800

    test

---
+++ src/main/java/com/gjx/gpms/GpMsApplication.java
@@ -0,0 +1,13 @@
+package com.gjx.gpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gjx/gpms/GpMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjx.gpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
