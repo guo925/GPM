11b81f8dba1f77ade9c6c9258b9b8b4502f0b78a
TenMountains <2151483295@qq.com>
2026-05-11 16:29:09 +0800

    test

---
+++ src/main/java/com/gjx/gpms/util/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.gjx.gpms.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey key;
+    private final long expiration;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret,
+                   @Value("${jwt.expiration}") long expiration) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.expiration = expiration;
+    }
+
+
+}
